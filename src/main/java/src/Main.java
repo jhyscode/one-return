39fd491976a1932b834ddf54aa2a8f5acb515c7c
e <1770613507@qq.com>
2023-01-05 14:38:21 +0800

    first

---
+++ src/main/java/src/Main.java
@@ -0,0 +1,13 @@
+package src;
+
+/**
+ * @author ：jhys
+ * @date ：Created in 2023/1/5 14:36
+ * @Description ：
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(111);
+        System.out.println("aaa");
+    }
+}
