adb280ddc191158b65605e639b787d2aa592cf6f
e <1770613507@qq.com>
2023-01-05 14:46:34 +0800

    add dev

---
+++ src/main/java/src/Main1.java
@@ -0,0 +1,13 @@
+package src;
+
+/**
+ * @author ：jhys
+ * @date ：Created in 2023/1/5 14:45
+ * @Description ：
+ */
+public class Main1 {
+    public static void main(String[] args) {
+        System.out.println("aaa");
+        System.out.println("ccc");
+    }
+}
